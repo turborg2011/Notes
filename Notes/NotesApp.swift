5e50b70bd3a97fdc8e90a5626a8593bc76c27563
Haydar <turborg2011@gmail.com>
2023-03-01 02:44:36 +0700

    Initial Commit

---
+++ Notes/NotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  NotesApp.swift
+//  Notes
+//
+//  Created by Хайдар Даукаев on 01.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct NotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
